19646e29cbc1286549bafd10d2a001ca3f22820d
Enda Lee <enda.lee@tudublin.ie>
2020-12-02 12:06:01 +0000

    link css

--- public/javascript/auth0-variables.example.js
+++ public/javascript/auth0-variables.example.js
@@ -1,9 +1,20 @@
-const AUTH0_CLIENT_ID = '';
-const AUTH0_DOMAIN = '.eu.auth0.com';
-const AUDIENCE = 'https://product-api';
+// Declare consts for Auth0 details reqired in this app
+
+
+// The Auth0 id for this app
+const AUTH0_CLIENT_ID = 'my client id - see Auth0 settings for this app';
+
+// Your Auth0 domain aka account/ tenant
+const AUTH0_DOMAIN = 'my-domain.eu.auth0.com';
+
+// Users of this app require access to the API, identified by...
+// This value is the 'Identifier' in your API settings 
+const AUDIENCE = 'https://api-identifier';
+
+// Where Auth0 should return the token to after authentication
 const AUTH0_CALLBACK_URL = 'http://localhost:3000';
 
-
+// Initialise Auth0 connection with parameters defined above
 const auth0WebAuth = new auth0.WebAuth({
   domain: AUTH0_DOMAIN,
   clientID: AUTH0_CLIENT_ID,
